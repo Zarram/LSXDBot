08135c3d1703e4631faaa97dd5c67db360bf28db
Zarram <thecakeisalie2801@gmail.com>
2026-04-14 16:05:50 +0200

    Update Index.py

--- Index.py
+++ Index.py
@@ -12,6 +12,7 @@ IMAGE_URLS = {
     '!congrats': 'https://media.discordapp.net/attachments/818907457253408768/1493600433615147018/angry-congrats.gif?ex=69df8f4d&is=69de3dcd&hm=6bf09ca8067087868d7c76967612fcb125a504f3dec40f816840bd8a6f498cac&=',
     '!login': 'https://media.discordapp.net/attachments/818907457253408768/1493600435485933680/login.gif?ex=69df8f4d&is=69de3dcd&hm=eebeeaaa6c12e669b2716624149899ef57af5b393eb77b2213a21485bbc3f1c1&=',
     '!mygm': 'https://media.discordapp.net/attachments/818907457253408768/1493600436588908735/mygm.png?ex=69df8f4d&is=69de3dcd&hm=b17ef794740230b9d227335d50960adfc0adc7d7faa642d153ea2c1c8cbddd39&=&format=webp&quality=lossless',
+    '!cinema': 'https://cdn.discordapp.com/attachments/1457739209606434846/1490516111115026603/GUZr4rVWwAAvGgi.png?ex=69df8b8d&is=69de3a0d&hm=e69e899678befe6a8487d5e428abfd45c9776a13a4ddb990be2fabe1181b8d14&',
 }
 
 
@@ -48,6 +49,12 @@ async def on_message(message):
 
     if message.content.startswith('!mygm'):
         await message.channel.send(IMAGE_URLS['!mygm'])
+
+    if message.content.startswith('!cinema'):
+        await message.channel.send(IMAGE_URLS['!cinema'])
+
+    if message.content.startswith('!raiddays'):
+        await message.channel.send('Wednesday and Monday 20-23 ServerTime! Optionally raid on Sundays as well!')
 
 #For security, the API key is stored in a separate file
 token = APIKey.DISCORD_API_KEY
